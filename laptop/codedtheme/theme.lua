ebe8f28234d8d035371750538fb92e1335691857
codedwrench <rick_1996@hotmail.com>
2016-05-31 10:31:18 +0200

    added awesomewmtheme

---
+++ laptop/codedtheme/theme.lua
@@ -0,0 +1,119 @@
+-------------------------------
+--  "Codedtheme" awesome theme  --
+--    By Codedwrench   --
+-------------------------------
+
+
+-- {{{ Main
+theme = {}
+theme.wallpaper = "/usr/share/awesome/themes/codedtheme/.wallpaper.jpg"
+theme.useless_gap_width = 10
+-- }}}
+
+-- {{{ Styles
+theme.font      = "Liberation Sans 9"
+
+-- {{{ Colors
+theme.fg_normal  = "#DDDDDD"
+theme.fg_focus   = "#7EB3B3"
+theme.fg_urgent  = "#CC9393"
+theme.bg_normal  = "#2E3030"
+theme.bg_focus   = "#1E1F1F"
+theme.bg_urgent  = "#1A130F"
+theme.bg_systray = theme.bg_normal
+theme.wibox_bg_normal = "#2E3030"
+
+
+-- }}}
+
+-- {{{ Borders
+theme.border_width  = 0
+theme.border_normal = "#1A130F"
+theme.border_focus  = "#6F6F6F"
+theme.border_marked = "#CC9393"
+-- }}}
+
+-- {{{ Titlebars
+theme.titlebar_bg_focus  = "#a2b8b6"
+theme.titlebar_bg_normal = "#1A130F"
+theme.tasklist_disable_icon = true
+-- }}}
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- titlebar_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- Example:
+theme.tasklist_bg_focus = "#2e3030"
+theme.tasklist_fg_focus = "#7eb3b3"
+-- }}}
+
+-- {{{ Widgets
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.fg_widget        = "#AECF96"
+--theme.fg_center_widget = "#88A175"
+--theme.fg_end_widget    = "#FF5656"
+--theme.bg_widget        = "#1A130F"
+--theme.border_widget    = "#1A130F"
+-- }}}
+
+-- {{{ Mouse finder
+theme.mouse_finder_color = "#CC9393"
+-- mouse_finder_[timeout|animate_timeout|radius|factor]
+-- }}}
+
+-- {{{ Menu
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_height = 15
+theme.menu_width  = 100
+-- }}}
+
+-- {{{ Icons
+-- {{{ Taglist
+theme.taglist_squares_sel   = "/usr/share/awesome/themes/codedtheme/taglist/squarefz.png"
+theme.taglist_squares_unsel = "/usr/share/awesome/themes/codedtheme/taglist/squarez.png"
+--theme.taglist_squares_resize = "false"
+-- }}}
+
+-- {{{ Misc
+theme.awesome_icon           = "/usr/share/awesome/themes/codedtheme/awesome-icon.png"
+theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
+-- }}}
+
+--  {{{ Layouts
+theme.layout_floating   = "/usr/share/awesome/themes/codedtheme/layouts/floating.png"
+-- }}}
+
+-- {{{ Titlebar
+theme.titlebar_close_button_focus  = "/usr/share/awesome/themes/codedtheme/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = "/usr/share/awesome/themes/codedtheme/titlebar/close_normal.png"
+
+theme.titlebar_ontop_button_focus_active  = "/usr/share/awesome/themes/codedtheme/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/codedtheme/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = "/usr/share/awesome/themes/codedtheme/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/codedtheme/titlebar/ontop_normal_inactive.png"
+
+theme.titlebar_sticky_button_focus_active  = "/usr/share/awesome/themes/codedtheme/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/codedtheme/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = "/usr/share/awesome/themes/codedtheme/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/codedtheme/titlebar/sticky_normal_inactive.png"
+
+theme.titlebar_floating_button_focus_active  = "/usr/share/awesome/themes/codedtheme/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/codedtheme/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = "/usr/share/awesome/themes/codedtheme/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/codedtheme/titlebar/floating_normal_inactive.png"
+
+theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/codedtheme/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/codedtheme/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/codedtheme/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/codedtheme/titlebar/maximized_normal_inactive.png"
+-- }}}
+-- }}}
+
+return theme
